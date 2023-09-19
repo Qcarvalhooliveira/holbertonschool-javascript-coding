7d6a21b257b72b2aff9dfc8f05aee3547deafb79
Qcarvalhooliveira <queisecarvalho@hotmail.com>
2023-09-19 20:29:33 +0200

    Readme

---
+++ 0-readme.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+const fs = require('fs');
+
+const filePath = process.argv[2];
+
+fs.readFile(filePath, 'utf-8', function (err, data) {
+  if (err) {
+    console.log(err);
+  } 
+    else {
+    console.log(data);
+  }
+});
