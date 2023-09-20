9c52223b260dbb3fa41e26a5f4fb63ee99c18797
Qcarvalhooliveira <queisecarvalho@hotmail.com>
2023-09-20 15:13:57 +0200

    How many completed?

---
+++ javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+const request = require('request');
+const url = process.argv[2];
+
+request(url, function (err, response, body) {
+  if (err) {
+    console.log(err);
+  }
+const tasksData = JSON.parse(body);
+const completedTasks = {};
+
+tasksData.forEach(task => {
+  if (task.completed) {
+    if (!completedTasks[task.userId]) {
+      completedTasks[task.userId] = 1;
+    } else {
+      completedTasks[task.userId]++;
+    }
+  }
+});
+  console.log(completedTasks);
+});
