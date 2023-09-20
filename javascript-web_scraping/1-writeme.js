d2cd0024d079dffb4fc280f3e578440fe35fc6d1
Qcarvalhooliveira <queisecarvalho@hotmail.com>
2023-09-20 11:14:09 +0200

    Write me

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -6,5 +6,5 @@ const filePath = process.argv[2];
 const textData = process.argv[3];
 
 fs.writeFile(filePath, textData, 'utf-8', function (err) {
-  if (err) {console.log(err); }
+  if (err) { console.log(err); }
 });
