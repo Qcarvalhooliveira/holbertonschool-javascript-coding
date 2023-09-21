126f40801db0d9dce3d72a2c8a67feb5c6b980a2
Qcarvalhooliveira <queisecarvalho@hotmail.com>
2023-09-21 14:30:37 +0200

    semistandard --fix

--- javascript-web_scraping/6-completed_tasks.js
+++ javascript-web_scraping/6-completed_tasks.js
@@ -6,17 +6,17 @@ request(url, function (err, response, body) {
   if (err) {
     console.log(err);
   }
-const tasksData = JSON.parse(body);
-const completedTasks = {};
+  const tasksData = JSON.parse(body);
+  const completedTasks = {};
 
-tasksData.forEach(task => {
-  if (task.completed) {
-    if (!completedTasks[task.userId]) {
-      completedTasks[task.userId] = 1;
-    } else {
-      completedTasks[task.userId]++;
+  tasksData.forEach(task => {
+    if (task.completed) {
+      if (!completedTasks[task.userId]) {
+        completedTasks[task.userId] = 1;
+      } else {
+        completedTasks[task.userId]++;
+      }
     }
-  }
-});
+  });
   console.log(completedTasks);
 });
