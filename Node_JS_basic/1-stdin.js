0bf29b23d1201b5fe1804f33a83ef393acea32d4
Qcarvalhooliveira <queisecarvalho@hotmail.com>
2023-10-08 12:18:52 +0200

    Using Process stdin

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,19 +1,15 @@
-const readline = require('readline');
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
+process.stdin.resume();
 
-console.log('Welcome to Holberton School, what is your name?');
-
-rl.on('line', (input) => {
-  const name = input.trim();
+process.stdin.on('readable', () => {
+  const name = process.stdin.read();
 
   if (name) {
-    console.log(`Your name is: ${name}`);
-    console.log('This important software is now closing');
-    rl.close();
-    process.exit(0); 
+    process.stdout.write(`Your name is: ${name}`);
   }
+});
+
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
 });
