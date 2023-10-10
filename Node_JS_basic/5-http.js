17ec600384961500614efab82cf6704d7c445e10
Qcarvalhooliveira <queisecarvalho@hotmail.com>
2023-10-10 18:43:05 +0200

    Fixing error

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -1,31 +1,28 @@
 const http = require('http');
-const countStudents = require('./3-read_file_async');
+const students = require('./3-read_file_async');
 
 const port = 1245;
 
 const app = http.createServer((req, res) => {
-  const { method, url } = req;
-  if (method === 'GET' && url === '/') {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.write('Hello Holberton School!');
-    res.end();
-  }
-  if (method === 'GET' && url === '/students') {
-    countStudents(String(process.argv.slice(2)))
-      .then((arrayOfClasses) => {
-        res.write('This is the list of our students\n');
-        for (const cls in arrayOfClasses) {
-          if (cls !== 'count') {
-            res.write(`Number of students in ${cls}: ${arrayOfClasses[cls].count}. List: ${arrayOfClasses[cls].names.join(', ')}\n`);
-          }
-        }
-        res.end();
-      })
-      .catch((err) => {
-        res.writeHead(500, { 'Content-Type': 'text/plain' });
-        res.end(`This is the list of our students\nCannot load the database\n${err.message}`);
-      });
+  switch (req.url) {
+    case '/':
+      res.writeHead(200);
+      res.end('Hello Holberton School!');
+      break;
+    case '/students':
+      res.writeHead(200);
+      res.write('This is the list of our students\n');
+      students(process.argv[2])
+        .then((data) => {
+          res.end(data);
+        })
+        .catch((error) => {
+          res.end(error.message);
+        });
+      break;
+    default:
+      res.writeHead(404);
+      res.end(JSON.stringify({ error: 'Resource not found' }));
   }
 });
 
