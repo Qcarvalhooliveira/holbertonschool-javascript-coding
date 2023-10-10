39a2198f7ec5e8f0a426901df37df87c0c2617fa
Qcarvalhooliveira <queisecarvalho@hotmail.com>
2023-10-10 17:49:34 +0200

    Fixing error

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -6,22 +6,24 @@ const port = 1245;
 const app = http.createServer((req, res) => {
   switch (req.url) {
     case '/':
-      res.writeHead(200);
+      res.writeHead(200, { 'Content-Type': 'text/plain' });
       res.end('Hello Holberton School!');
       break;
     case '/students':
-      res.writeHead(200);
-      res.write('This is the list of our students\n');
       students(process.argv[2])
         .then((data) => {
-          res.end(data);
+          res.writeHead(200, { 'Content-Type': 'text/plain' });
+          res.write('This is the list of our students\n');
+          res.write(data);  // Writing the data directly (assuming it's plain text)
+          res.end();
         })
         .catch((error) => {
-          res.end(error.message);
+          res.writeHead(500, { 'Content-Type': 'text/plain' });
+          res.end(`Error: ${error.message}`);
         });
       break;
     default:
-      res.writeHead(404);
+      res.writeHead(404, { 'Content-Type': 'application/json' });
       res.end(JSON.stringify({ error: 'Resource not found' }));
   }
 });
