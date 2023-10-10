476323c03669d9ec710053a2d36f4f789882edf6
Qcarvalhooliveira <queisecarvalho@hotmail.com>
2023-10-10 14:21:45 +0200

    Create a small HTTP server using Nodes HTTP module

--- Node_JS_basic/4-http.js
+++ Node_JS_basic/4-http.js
@@ -1,13 +1,15 @@
 const http = require('http');
 
 const app = http.createServer((req, res) => {
-	  res.writeHead(200, { 'Content-Type': 'text/plain' });
-	  res.end('Hello Holberton School!\n');
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello Holberton School!\n');
 });
 
-app.listen(1245, () => {
-	  console.log('Server is listening on port 1245');
+const port = 1245;
+const hostname = '127.0.0.1';
+
+app.listen(port, hostname, () => {
+  console.log(`Server is listening on http://${hostname}:${port}/`);
 });
 
 module.exports = app;
-
