0ae1c496b459d48235d21f2c749937341952023e
Qcarvalhooliveira <queisecarvalho@hotmail.com>
2023-10-13 12:58:53 +0200

    Reading a file asynchronously with Node JS

--- Node_JS_basic/3-read_file_async.js
+++ Node_JS_basic/3-read_file_async.js
@@ -7,30 +7,30 @@ function countStudents(path) {
         reject(new Error('Cannot load the database'));
       } else {
         const lines = data.split('\n').filter((line) => line.trim() !== '');
-        if (lines.length === 1 && lines[0].trim() === '') {
-          resolve([]); // Database is empty
-        } else {
-          const counters = {};
 
-          for (let i = 1; i < lines.length; i += 1) {
-            const [firstName, , , field] = lines[i].split(',');
-            if (field) {
-              counters[field] = counters[field] || { count: 0, names: [] };
-              counters[field].count += 1;
-              counters[field].names.push(firstName.trim());
-            }
+        console.log(`Number of students: ${lines.length - 1}`);
+
+        const counters = {};
+
+        for (let i = 1; i < lines.length; i += 1) {
+          const [firstName, , , field] = lines[i].split(',');
+          if (field) {
+            counters[field] = counters[field] || { count: 0, names: [] };
+            counters[field].count += 1;
+            counters[field].names.push(firstName.trim());
           }
-
-          const studentsInfo = Object.keys(counters).map((field) => {
-            return `Number of students in ${field}: ${counters[field].count}. List: ${counters[field].names.join(', ')}`;
-          });
-
-          resolve(studentsInfo);
         }
+
+        for (const field in counters) {
+          if (Object.prototype.hasOwnProperty.call(counters, field)) {
+            console.log(`Number of students in ${field}: ${counters[field].count}. List: ${counters[field].names.join(', ')}`);
+          }
+        }
+
+        resolve();
       }
     });
   });
 }
 
 module.exports = countStudents;
-
